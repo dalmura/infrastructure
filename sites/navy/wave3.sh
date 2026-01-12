3a629857bd692c5d83bcd8088ee1673891dfe0cd
andres <andres.chaves@westworld.com>
2026-01-12 21:15:44 +1100

    removing some apps from wave5

--- sites/navy/wave3.sh
+++ sites/navy/wave3.sh
@@ -15,3 +15,19 @@ argocd app sync wave-3
 
 # Deploy the child applications
 argocd app sync -l app.kubernetes.io/instance=wave-3
+
+vault auth enable oidc
+
+vault write auth/oidc/config \
+    oidc_discovery_url="https://auth.navy.dalmura.cloud/application/o/vault/" \
+    oidc_client_id="<Client ID from above>" \
+    oidc_client_secret="<Client Secret from above>" \
+    default_role="default"
+
+vault write auth/oidc/role/default \
+    allowed_redirect_uris="https://vault.navy.dalmura.cloud/ui/vault/auth/oidc/oidc/callback" \
+    allowed_redirect_uris="https://vault.navy.dalmura.cloud/oidc/callback" \
+    allowed_redirect_uris="http://localhost:8250/oidc/callback" \
+    user_claim="preferred_username" \
+    groups_claim="roles" \
+    oidc_scopes="openid,profile,email,entitlements"
